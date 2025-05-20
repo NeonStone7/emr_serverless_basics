0e7958c06105aedfddce5f30362139c63af769e7
my_user <oamenmodupe@gmail.com>
2025-05-20 16:54:56 +0100

    added iceberg

--- spark_job/spark_processor.py
+++ spark_job/spark_processor.py
@@ -1,9 +1,14 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = (SparkSession.builder 
-    .appName("Data Processing") 
-    .getOrCreate() ) 
+spark = (
+    SparkSession.builder
+    .appName("Iceberg Parquet Writer")
+    .config("spark.sql.catalog.my_catalog", "org.apache.iceberg.spark.SparkCatalog")
+    .config("spark.sql.catalog.my_catalog.type", "hadoop")
+    .config("spark.sql.catalog.my_catalog.warehouse", "s3://emr-project-raw/serverless_example/iceberg/")
+    .getOrCreate()
+)
 
 # data_path = 'data/data.csv'
 data_path = "s3://emr-project-raw/serverless_example/data/data.csv"
@@ -14,5 +19,7 @@ df = (
       .withColumn('balance_eurocents', regexp_replace((col('balance')/100).cast('string'), '.', ','))
 )
 
+df.writeTo("my_catalog.db.processed_data").using("iceberg").createOrReplace()
+
 # df.write.format('iceberg').mode('overwrite').save('s3://emr-project-raw/serverless_example/iceberg/processed_data')
-df.write.option('header', True).csv('s3://emr-project-raw/serverless_example/iceberg/processed_data', mode='overwrite')
+# df.write.option('header', True).csv('s3://emr-project-raw/serverless_example/iceberg/processed_data', mode='overwrite')
